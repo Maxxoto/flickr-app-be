2415e120e131db1e970d5c34924cc0cae61dad06
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2022-04-08 04:24:33 +0700

    [IMP] add photo routes

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,1 +1,10 @@
-module.exports = (app) => {};
+const getFlickrPublicPhotos = require('../services/flickrService');
+
+module.exports = (app) => {
+  app.get('/api/photos', (req, res) => {
+    // TODO : run flickrService
+    // TODO : Param check for user , if user make a search request
+    // TODO : transform response flickrService to our response format
+    res.send('Hello All !');
+  });
+};
