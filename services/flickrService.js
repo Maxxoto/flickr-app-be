5b3b009856aaeeae05c63ad3fd8ddbaa73e655dc
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2022-04-08 21:10:09 +0700

    [IMP] add flickr response transformer

--- services/flickrService.js
+++ services/flickrService.js
@@ -1,14 +1,27 @@
 const jsonpClient = require('jsonp-client');
 const util = require('util');
 
-const getFlickrPublicPhotos = () => {
+const getFlickrPublicPhotos = (tags_query) => {
   const jsonpClientNew = util.promisify(jsonpClient);
-  const response = jsonpClientNew(process.env.FLICK_PUBLIC_PHOTOS_ENDPOINT)
+
+  let url = process.env.FLICK_PUBLIC_PHOTOS_ENDPOINT;
+
+  if (tags_query) {
+    url = url + '&tags=' + tags_query;
+    // Use most relevant tags mode by selecting tagmode = all
+    url = url + '&tagmode=all';
+  }
+  console.debug('URL run : ' + url);
+
+  const response = jsonpClientNew(url)
     .then((data) => {
       return data;
     })
     .catch((err) => {
-      throw new Error(err);
+      return {
+        error: 'Error while fetching flickr public photos ',
+        msg: err,
+      };
     });
   return response;
 };
