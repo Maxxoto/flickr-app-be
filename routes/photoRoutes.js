3ba587d97ffe9e6f23346641354a53d16aadcbe3
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2022-04-08 21:09:55 +0700

    [IMP] implement photo routes to flickr response transformer

--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,10 +1,32 @@
-const getFlickrPublicPhotos = require('../services/flickrService');
+const flickrService = require('../services/flickrService');
+
+const responseFormatter = require('../utils/responseFormatter');
+const responseTransformer = require('../utils/responseTransformer');
 
 module.exports = (app) => {
-  app.get('/api/photos', (req, res) => {
-    // TODO : run flickrService
-    // TODO : Param check for user , if user make a search request
-    // TODO : transform response flickrService to our response format
-    res.send('Hello All !');
+  app.get('/api/photos', async (req, res) => {
+    // Extract only tags from query string
+    const tags_query = req.query.tags;
+    console.log(tags_query);
+
+    const flickrResponse = await flickrService.getFlickrPublicPhotos(
+      tags_query,
+    );
+
+    if (flickrResponse.error) {
+      console.error(flickrResponse);
+      res
+        .status(400)
+        .send(
+          responseFormatter(
+            400,
+            flickrResponse.error,
+            flickrResponse.msg.toString(),
+          ),
+        );
+    } else {
+      const formattedFlickrResponse = responseTransformer(flickrResponse);
+      res.send(responseFormatter(200, 'Success', formattedFlickrResponse));
+    }
   });
 };
